ee9730e96c2544db1ebd4b35157f75cb707c48ee
alessandraGudoy <gudoyalessandra@gmail.com>
2024-11-18 11:27:43 -1000

    starting student home page

--- src/components/StuffItem.tsx
+++ src/components/StuffItem.tsx
@@ -1,16 +1,21 @@
 import { Stuff } from '@prisma/client';
+import { Container } from 'react-bootstrap';
 import Link from 'next/link';
 
 /* Renders a single row in the List Stuff table. See list/page.tsx. */
-const StuffItem = ({ name, quantity, condition, id }: Stuff) => (
-  <tr>
-    <td>{name}</td>
-    <td>{quantity}</td>
-    <td>{condition}</td>
-    <td>
+const StuffItem = ({ name, skills, professionalPage, id }: Student) => (
+  <Container fluid>
+    <Container>
+      <h4>{name}</h4>
+      <p>{skills}</p>
+      <p>
+        <a href={professionalPage} />
+      </p>
       <Link href={`/edit/${id}`}>Edit</Link>
-    </td>
-  </tr>
+
+      
+    </Container>
+  </Container>
 );
 
 export default StuffItem;
