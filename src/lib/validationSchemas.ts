1602a9cad3b746d9f8e1ac1d94d4477a68e6a0a8
alessandraGudoy <gudoyalessandra@gmail.com>
2024-11-19 21:19:40 -1000

    added schemas to add/edit student/company, edited sign up form for student

--- src/lib/validationSchemas.ts
+++ src/lib/validationSchemas.ts
@@ -1,5 +1,41 @@
 import * as Yup from 'yup';
 
+export const AddStudentSchema = Yup.object({
+  name: Yup.string().required(),
+  skills: Yup.string().required(),
+  location: Yup.string().required(),
+  professionalPage: Yup.string().required(),
+  owner: Yup.string().required(),
+});
+
+export const EditStudentSchema = Yup.object({
+  id: Yup.number().required(),
+  name: Yup.string().required(),
+  skills: Yup.string().required(),
+  location: Yup.string().required(),
+  professionalPage: Yup.string().required(),
+  owner: Yup.string().required(),
+});
+
+export const AddCompanySchema = Yup.object({
+  name: Yup.string().required(),
+  overview: Yup.string().required(),
+  location: Yup.string().required(),
+  // links: company.links,
+  // emails: company.emails,
+  owner: Yup.string().required(),
+});
+
+export const EditCompanySchema = Yup.object({
+  id: Yup.number().required(),
+  name: Yup.string().required(),
+  overview: Yup.string().required(),
+  location: Yup.string().required(),
+  // links: company.links,
+  // emails: company.emails,
+  owner: Yup.string().required(),
+});
+
 export const AddStuffSchema = Yup.object({
   name: Yup.string().required(),
   quantity: Yup.number().positive().required(),
