6cd0f28f4162bb239dcda32454b69fed5805d7ad
Amy Shin <yoolimshin@gmail.com>
2024-11-20 13:57:31 -1000

    work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
 import { Col, Container, Row } from 'react-bootstrap';
-=======
-import { Col, Container, Image, Row } from 'react-bootstrap';
->>>>>>> parent of 5973a63 (add)
 
 /** The Home page. */
 const Home = () => (
   <main>
-<<<<<<< HEAD
     <Container id="landing-page" fluid className="d-flex align-items-center justify-content-center py-5">
       <Row className="justify-content-center text-center">
         <Col xs={10} md={8} lg={6}>
@@ -34,17 +29,6 @@ const Home = () => (
               to join us now!
             </strong>
           </h4>
-=======
-    <Container id="landing-page" fluid className="py-3">
-      <Row className="align-middle text-center">
-        <Col xs={4}>
-          <Image src="next.svg" width="150px" alt="" />
-        </Col>
-
-        <Col xs={8} className="d-flex flex-column justify-content-center">
-          <h1>Welcome to this template</h1>
-          <p>Now get to work and modify this app!</p>
->>>>>>> parent of 5973a63 (add)
         </Col>
       </Row>
     </Container>
