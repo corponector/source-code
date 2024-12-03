2ad942d8f4a4c45d1ce960124cab4bdc301f0859
Alexis Karl Buted <akbuted@hawaii.edu>
2024-12-02 16:16:10 -1000

    i think company image input works

---
+++ src/app/company/edit/[id]/page.tsx
@@ -0,0 +1,35 @@
+import { getServerSession } from 'next-auth';
+import { notFound } from 'next/navigation';
+import { Company } from '@prisma/client';
+import authOptions from '@/lib/authOptions';
+import { loggedInProtectedPage } from '@/lib/page-protection';
+import { prisma } from '@/lib/prisma';
+import { Container } from 'react-bootstrap';
+import EditCompanyForm from '@/components/EditStudentForm';
+
+export default async function EditStuffPage({ params }: { params: { id: string | string[] } }) {
+  // Protect the page, only logged in users can access it.
+  const session = await getServerSession(authOptions);
+  loggedInProtectedPage(
+    session as {
+      user: { email: string; id: string; randomKey: string };
+      // eslint-disable-next-line @typescript-eslint/comma-dangle
+    } | null,
+  );
+  const id = Number(Array.isArray(params?.id) ? params?.id[0] : params?.id);
+  // console.log(id);
+  const company: Company | null = await prisma.company.findUnique({
+    where: { id },
+  });
+  if (!company) {
+    return notFound();
+  }
+
+  return (
+    <main>
+      <Container>
+        <EditCompanyForm company={company} />
+      </Container>
+    </main>
+  );
+}
---
+++ src/components/EditCompanyForm.tsx
@@ -0,0 +1,117 @@
+'use client';
+
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
+import swal from 'sweetalert';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Company } from '@prisma/client';
+import { EditCompanySchema } from '@/lib/validationSchemas';
+import { editCompany } from '@/lib/dbActions';
+
+const onSubmit = async (data: {
+  id: number;
+  name: string;
+  location: string;
+  overview: string;
+  links: string;
+  emails: string;
+  profileImage: string;
+  owner: string;
+}) => {
+  await editCompany(data);
+  swal('Success', 'Your company has been updated', 'success', {
+    timer: 2000,
+  });
+};
+
+const EditCompanyForm = ({ company }: { company: Company }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(EditCompanySchema),
+  });
+
+  return (
+    <Container className="py-3">
+      <h2>Edit Company</h2>
+
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <input type="hidden" {...register('id')} value={company.id} />
+
+        {/* Name & Location Input */}
+        <Row>
+          <Col>
+            <Form.Group className="mb-3">
+              <Form.Label>Company Name</Form.Label>
+              <Form.Control type="text" {...register('name')} isInvalid={!!errors.name} defaultValue={company.name} />
+              <Form.Control.Feedback type="invalid">{errors.name?.message}</Form.Control.Feedback>
+            </Form.Group>
+          </Col>
+
+          <Col>
+            <Form.Group className="mb-3">
+              <Form.Label>Location</Form.Label>
+              <Form.Control
+                type="text"
+                {...register('location')}
+                isInvalid={!!errors.location}
+                defaultValue={company.location}
+              />
+              <Form.Control.Feedback type="invalid">{errors.location?.message}</Form.Control.Feedback>
+            </Form.Group>
+          </Col>
+        </Row>
+
+        {/* Overview Input */}
+        <Form.Group className="mb-3">
+          <Form.Label>Overview</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={3}
+            {...register('overview')}
+            isInvalid={!!errors.overview}
+            defaultValue={company.overview}
+          />
+          <Form.Control.Feedback type="invalid">{errors.overview?.message}</Form.Control.Feedback>
+        </Form.Group>
+
+        {/* Links Input */}
+        <Form.Group className="mb-3">
+          <Form.Label>Links (Website or Social Media)</Form.Label>
+          <Form.Control type="url" {...register('links')} isInvalid={!!errors.links} defaultValue={company.links} />
+          <Form.Control.Feedback type="invalid">{errors.links?.message}</Form.Control.Feedback>
+        </Form.Group>
+
+        {/* Emails Input */}
+        <Form.Group className="mb-3">
+          <Form.Label>Contact Email</Form.Label>
+          <Form.Control
+            type="email"
+            {...register('emails')}
+            isInvalid={!!errors.emails}
+            defaultValue={company.emails}
+          />
+          <Form.Control.Feedback type="invalid">{errors.emails?.message}</Form.Control.Feedback>
+        </Form.Group>
+
+        {/* Profile Image URL Input */}
+        <Form.Group className="mb-3">
+          <Form.Label>Profile Image URL</Form.Label>
+          <Form.Control type="url" {...register('profileImage')} isInvalid={!!errors.profileImage} />
+          <Form.Control.Feedback type="invalid">{errors.profileImage?.message}</Form.Control.Feedback>
+        </Form.Group>
+
+        <input type="hidden" {...register('owner')} value={company.owner} />
+
+        {/* Submit Button */}
+        <Button type="submit" variant="primary">
+          Submit
+        </Button>
+      </Form>
+    </Container>
+  );
+};
+
+export default EditCompanyForm;
