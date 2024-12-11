7e83c7f5e2b69cd03333a01bbfa63e1751097712
Justine Ponce <poncej@hawaii.edu>
2024-12-11 07:57:12 -1000

    Adjust CompanyProfile Cards

--- src/components/CompanyProfile.tsx
+++ src/components/CompanyProfile.tsx
@@ -5,6 +5,7 @@
 import React from 'react';
 import { Container, Card, Image, Row, Col } from 'react-bootstrap';
 import { Company } from './Interface';
+import CompanyPositions from './CompanyPositions';
 
 interface CompanyProfileProps {
   company: Company;
@@ -12,9 +13,9 @@ interface CompanyProfileProps {
 
 const CompanyProfile: React.FC<CompanyProfileProps> = ({ company }) => (
   <Container fluid className="d-flex justify-content-center align-items-center vh-100">
-    <Card className="w-75 h-75" style={{ backgroundColor: 'rgba(999, 999, 999, 0.5)', border: 'none' }}>
+    <Card className="w-75" style={{ backgroundColor: 'rgba(999, 999, 999, 0.5)', border: 'none' }}>
       <Card.Body>
-        <Row className="h-100">
+        <Row>
           <Col md={3} className="d-flex flex-column justify-content-center align-items-center">
             <Image
               src={company.profileImage || '/default-company.svg'}
@@ -29,7 +30,7 @@ const CompanyProfile: React.FC<CompanyProfileProps> = ({ company }) => (
               Apply Now on Our Website!
             </a>
           </Col>
-          <Col md={6} className="d-flex flex-column justify-content-center">
+          <Col md={9} className="d-flex flex-column justify-content-center">
             <h1 className="display-4">{company.name}</h1>
             <p>
               <strong>Location:</strong>
@@ -46,48 +47,16 @@ const CompanyProfile: React.FC<CompanyProfileProps> = ({ company }) => (
               {' '}
               {Array.isArray(company.emails) ? company.emails.join(', ') : company.emails}
             </p>
+            <p>
+              <strong>Links:</strong>
+              {' '}
+              {Array.isArray(company.links) ? company.links.join(', ') : company.links}
+            </p>
           </Col>
-          <Col md={3} className="d-flex flex-column justify-content-center">
-            <h3 className="display-7">Open Positions:</h3>
-            <ul className="list-unstyled">
-              {company.positions.map((position) => (
-                <li key={position.id} className="mb-3">
-                  <strong>
-                    •
-                    {' '}
-                    {position.title}
-                  </strong>
-                  <p>{position.description}</p>
-                  <p>
-                    <strong>
-                      -
-                      Skills:
-                    </strong>
-                    {' '}
-                    {Array.isArray(position.skills) ? position.skills.join(', ') : position.skills}
-                  </p>
-                  <p>
-                    <strong>
-                      -
-                      Job Type:
-                    </strong>
-                    {' '}
-                    {Array.isArray(position.jobType) ? position.jobType.join(', ') : position.jobType}
-                  </p>
-                  <p>
-                    <strong> - Number of Hires:</strong>
-                    {' '}
-                    {position.numberOfHires}
-                  </p>
-                  <p>
-                    <strong> - Salary Range:</strong>
-                    {' '}
-                    $
-                    {position.salaryRange}
-                  </p>
-                </li>
-              ))}
-            </ul>
+        </Row>
+        <Row className="mt-4">
+          <Col>
+            <CompanyPositions positions={company.positions} />
           </Col>
         </Row>
       </Card.Body>
